0e8850b60a16588b092df59a66abedfca4c9d038
Kevin <kev.chu.pro@gmail.com>
2021-11-30 10:44:04 -0800

    feat: add boilerplate for potion abc

---
+++ potion.py
@@ -0,0 +1,17 @@
+# Kevin's Time Tracker: 0.5 hour
+
+import abc
+
+class Potion(metaclass = abc.ABCMeta):
+
+    @abc.abstractmethod
+    def __str__(self):
+        return f'{self.name}'
+
+    @abc.abstractmethod
+    def action(self):
+        pass
+
+    @abc.abstractproperty
+    def name(self):
+        pass
