79d09fad2587421cb2a4652737caa92cc98b58de
308241561 <huangxingguo1986@gmail.com>
2021-12-02 11:31:28 -0800

    basic logic draft of the main and map py

--- main.py
+++ main.py
@@ -1,3 +1,53 @@
+from adventurer import Adventurer
+from map import Map
+from dungeon import Dungeon
+
+
 class Main:
     def __init__(self):
         pass
+    def startMainMenu(self):
+        # show the title info(text), group info, basic instructions(h --help),(e -exit)
+        # h--help
+        # wsad-- u d l f
+        # i for player current info health, pillar...
+        # p for health pots
+        # v for vision pots
+        print("instruction") # read a text file
+        print("please enter command")
+        while True:
+            command = input("")
+            if command == 'h':
+                self.display_basic_commands('h')
+                continue
+            elif command == 'e':
+                break
+            else:
+                self.startGame()
+            # handle exceptions
+
+    def startGame(self):
+        pass
+        # create 1, 2, 3
+        # get user command
+
+    def move(self):
+        # adventurer interates with dun
+        # if alive let the user to interacte if dead, exit
+        # if adv at the exit? if adv has all the pillars?
+        # add the traveled room to the list and display to the map
+        pass
+
+    def display_basic_commands(self):
+        # print out ....   test it works correctly
+        pass
+    def display_player_info(self):
+        playe_name = input("Welcome to Dnd, Please enter your player's name: ")
+        pass
+    def move(self):
+        pass
+
+
+if __name__ == "__main__": # when this file is run,
+    main = Main()
+    main.start()
--- map.py
+++ map.py
@@ -1,3 +1,5 @@
 class Map:
     def __init__(self):
         pass
+
+    # mentain a 2D array to reveal the fog of war on the dungeon
