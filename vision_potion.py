d28dbcad7ef70211a50aac662f85f3be69e9ea25
Kevin <kev.chu.pro@gmail.com>
2021-12-07 17:09:37 -0800

    feat: add brainstorm functionality

--- vision_potion.py
+++ vision_potion.py
@@ -1,18 +1,39 @@
+# Kevin's Time Tracker: 1 hours
+
 from potion import Potion
+from random import randint
 
 class VisionPotion(Potion):
-    def __init__(self):
-        self.name = 'vision_pot'
+    """Vision potion that an adventurer can use to reveal surrounding rooms
+    """
+    def __init__(self, extra_effects = False):
+        self.__name = 'Vision Potion'
+        self.__rooms_revealed = 8       #Default value, for surrounding rooms revealed
+        if extra_effects:
+            self.__potion_effect()
 
     def __str__(self):
-        return f'{self.name}'
+        return f'Item: {self.name}\nEffect: {self.rooms_revealed} adjacent rooms are revealed.'
 
     def action(self):
-        pass
+        return self.rooms_revealed
 
-    def random(self):
-        pass
+    def __potion_effect(self):
+        self.rooms_revealed = 0
+        potion_strength = randint(1, 8)
+        self.rooms_revealed += potion_strength
 
     @property
     def name(self):
-        return self.name
+        return self.__name
+
+    @property
+    def rooms_revealed(self):
+        return self.__rooms_revealed
+
+    @rooms_revealed.setter
+    def rooms_revealed(self, value):
+        if isinstance(value, int):
+            self.__rooms_revealed = value
+        else:
+            raise ValueError("Value passed into method was not an integer value.")
