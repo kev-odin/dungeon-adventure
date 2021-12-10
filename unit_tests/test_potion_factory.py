a500cde50e51999917c4ad5a42ff9147003d9d64
Kevin <kev.chu.pro@gmail.com>
2021-12-10 15:44:25 -0800

    test: add type checking tests

--- unit_tests/test_potion_factory.py
+++ unit_tests/test_potion_factory.py
@@ -1,11 +1,15 @@
+# Kevin's Time Tracker: 1 hour
+
 from potion_factory import PotionFactory
+from health_potion import HealthPotion
+from vision_potion import VisionPotion
 import unittest
 
 class PotionFactoryTest(unittest.TestCase):
     def test_create_health_potion(self):
         test_health = PotionFactory.create_potion("health")
-        self.assertEqual(test_health.name, "Health Potion")
+        assert isinstance(test_health, HealthPotion)
     
     def test_create_vision_potion(self):
         test_vision = PotionFactory.create_potion("vision")
-        self.assertEqual(test_vision.name, "Vision Potion")
+        assert isinstance(test_vision, VisionPotion)
