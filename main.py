9f6f60f92f1cd6af9693d4d9d789d925b6e4532b
308241561 <huangxingguo1986@gmail.com>
2021-12-09 17:34:56 -0800

    can move around the dungeon and print the traveled rooms

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 '''
-Time used: 13 hours
+Time used: 18 hours
 xingguo
 '''
 
@@ -25,63 +25,65 @@ class Main:
     '''
     main method
     '''
-    # what to do with the init?
-    # if do GUI
-    # to create
     def __init__(self):
         self.visited_array = []
         self.create_visited_room_array()
 
-
-
-
     def game_flow(self):
-        # 1st menu:
-            # show the title info(text), group info, basic instructions(h --help),(e -exit)
-        # 2nd menu:
-            # wsad-- u d l f
-            # i for player current info health, pillar...
-            # p for health pots
-            # v for vision pots
-
         print("\nWelcome! \nprint game logo/rules/info here \n")  # read a text file to display the game logo
-        # while True:
-        #     help_or_continue = input("Please enter \'h\' for all commands of the game, or \'q\' to quit the game: ")
-        #     if help_or_continue == 'h':
-        #         print("\nComplete commands of the game: \n"
-        #               "Movement commands: \n"
-        #               "\tw: move up\n"
-        #               "\ts: move down\n"
-        #               "\ta: move left\n"
-        #               "\td: move right\n"
-        #               "Status commands: \n"
-        #               "\ti: show adventurer info\n"
-        #               "\tr: show current room\n"
-        #               "\tm: show adventurer map\n"
-        #               "Item commands: \n"
-        #               "\tp: use health potion\n"
-        #               "\tv: use vision potion\n"
-        #               "Other commands: \n"
-        #               "\tq: quit the game\n"
-        #               "----to be continue----\n")
-        #         break
-        #     elif help_or_continue == 'q':
-        #         print("Thanks,Bye!")
-        #         break
-        #     else:
-        #         continue
+        while True:
+            help_or_continue = input("Please enter:\n \'h\' : for all commands of the game\n \'c\' : for continue \n \'q\' : for quit the game: ")
+            if help_or_continue == 'h':
+                print("\nComplete commands of the game: \n"
+                      "Movement commands: \n"
+                      "\tw: move up\n"
+                      "\ts: move down\n"
+                      "\ta: move left\n"
+                      "\td: move right\n"
+                      "Status commands: \n"
+                      "\ti: show adventurer info\n"
+                      "\tr: show current room\n"
+                      "\tm: show adventurer map\n"
+                      "Item commands: \n"
+                      "\tp: use health potion\n"
+                      "\tv: use vision potion\n"
+                      "Other commands: \n"
+                      "\tq: quit the game\n"
+                      "----to be continue----\n")
+                break
+            elif help_or_continue == 'q':
+                print("\tThanks,Bye!")
+                break
+            elif help_or_continue == 'c':
+                print("TODO It should continue to the game!")
+            else:
+                continue
         adventurer_name = input("Now please enter a name for your adventurer: ")
         desired_difficulty = input("Now please enter a difficulty level(easy/medium/hard/inhumane): ")
         print("Following is your game, good luck!")
         # Creates a Dungeon Object and
         db = DungeonBuilder(desired_difficulty)
         built = db.build_dungeon()
-        # completeDungeon = Dungeon(dungeon = built)  # now we have done creating a dungeon, correct?
+
         # Creates a Adventurer Object
-        adv = Adventurer(adventurer_name, desired_difficulty)
+        adventurer = Adventurer(adventurer_name, desired_difficulty)
         self.set_visited_room(built.entrance[0],built.entrance[1])
         print(built.get_visible_dungeon_string(self.visited_array)) # print visited room
         print(built.get_visible_dungeon_string()) # print whole dungeon
+        while True:
+            move = input("Please input your move: ")
+            new_room = built.move_adventurer(move)
+            print(
+                  "new_room health potion: " + str(new_room.health_potion) + "\n" +
+                  "new_room vision potion: " + str(new_room.vision_potion) + "\n" +
+                  "new_room pit damage: " + str(new_room.pit_damage) + "\n" +
+                  "new_room contents in general: " + str(new_room.contents)
+                )
+
+            self.set_visited_room(built.adventurer_loc[0], built.adventurer_loc[1])
+            print(built.get_visible_dungeon_string(self.visited_array))
+
+        adv = Adventurer(adventurer_name, desired_difficulty)  # repeat creating the adventurer
         while True:
             if adv.is_alive() and True and True: # if 1. alive 2. get the 4 pillars  3.arrived at the exit
                 print("Congrats! you escaped the dungeon successfully")
@@ -138,7 +140,7 @@ class Main:
         pass
 
     def create_visited_room_array(self):
-        for row in range(0, 5): # how to find out the size,change in the future
+        for row in range(0, 5): # how to find out the size,change in the future  5x5;8x8;10x10
             self.visited_array.append([])
             for col in range(0, 5):
                 self.visited_array[row].append([])
