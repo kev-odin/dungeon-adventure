3beb3c94a6c83ae790bd7832fcc0490f25ab5f7d
Kevin <kev.chu.pro@gmail.com>
2021-12-10 19:54:42 -0800

    test: add comprehensive tests

--- unit_tests/test_potion_factory.py
+++ unit_tests/test_potion_factory.py
@@ -1,4 +1,4 @@
-# Kevin's Time Tracker: 1 hour
+# Kevin's Time Tracker: 1.5 hours
 
 from potion_factory import PotionFactory
 from health_potion import HealthPotion
@@ -13,3 +13,22 @@ class PotionFactoryTest(unittest.TestCase):
     def test_create_vision_potion(self):
         test_vision = PotionFactory.create_potion("vision")
         assert isinstance(test_vision, VisionPotion)
+
+    def test_create_blank_potion(self):
+        try:
+            test_blank = PotionFactory.create_potion()
+        except TypeError:
+            self.assertRaises(TypeError)
+    
+    def test_create_invalid_potion(self):
+        try:
+            test_invalid = PotionFactory.create_potion("super")
+        except ValueError:
+            self.assertRaises(ValueError)
+
+    def test_create_numerous_potions(self):
+        multiple = ("health", "vision")
+        try:
+            test_numerous = PotionFactory.create_potion(multiple)
+        except TypeError:
+            self.assertRaises(TypeError)
