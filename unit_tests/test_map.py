174ee626b26e82adaf4a0467c93c258d84b1190c
308241561 <huangxingguo1986@gmail.com>
2021-12-13 13:30:22 -0800

    begin test classes

---
+++ unit_tests/test_map.py
@@ -0,0 +1,13 @@
+import unittest
+from map import Map
+
+class TestMap(unittest.TestCase):
+
+    def test_init(self):
+        pass
+    def test_create_visited_room_array(self):
+        pass
+    def test_set_visited_room(self):
+        pass
+    def test_visited_array(self):
+        pass
