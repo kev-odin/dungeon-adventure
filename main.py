23b6e1a4befd6a7478ad5ad94f3b011754da197f
308241561 <huangxingguo1986@gmail.com>
2021-12-13 10:39:32 -0800

    corner cases taken cared of. Now it should not break easily.

--- main.py
+++ main.py
@@ -30,8 +30,8 @@ ____
 |  _  | _| | _ _  ___  ___ | |_  _ _  ___  ___ 
 |     || . || | || -_||   ||  _|| | ||  _|| -_|
 |__|__||___| \_/ |___||_|_||_|  |___||_|  |___|  
-Welcome to Dungeon Adventure! 
 
+Welcome to Dungeon Adventure! 
 Your goal: 
      1⃣ collect all the four pillars of OOP, 
      2⃣ get to the exit safely. 
@@ -40,16 +40,16 @@ Items randomly placed in each room:
      1⃣ Healing potion(random heal amount),
      2⃣ Vision potion(reveal adjacent room), 
      3⃣ Pit(random damage to adventurer), 
-     4⃣ OOP pillars("A","P","I","E").
-     
+     4⃣ OOP pillars("A","P","I","E").    
 Good luck!!! 
                 """
         print(welcome_page)
         bool_flag = True
         while bool_flag:
             help_or_continue = input(
-                                     "\'H\' : for all commands of the game\n"
-                                     "\'C\' : for continue \nPlease enter your choice: "
+                                     "\'h\' : for all commands of the game\n"
+                                     "\'c\' : for continue \n"
+                                     "Your choice: "
                                     )
             if help_or_continue.lower() == 'h':
                 self.print_complete_menu()
@@ -61,8 +61,14 @@ Good luck!!!
                 continue  # invalid input
 
         # obtain name and difficulty level from player
-        adventurer_name = input("Now please enter a name for your adventurer: ")
-        desired_difficulty = input("Now please enter a difficulty level(easy/medium/hard/inhumane): ").lower()
+        adventurer_name = input("Choose an adventurer name: ")
+
+        while True:
+            desired_difficulty = input("Choose a difficulty level(easy/medium/hard/inhumane): ").lower()
+            if desired_difficulty in ("easy","medium", "hard", "inhumane"):
+                break
+            else:
+                continue
 
         print("\nYour adventurer "+adventurer_name+" is ready. Good luck!")
 
@@ -88,58 +94,60 @@ Good luck!!!
         # print(dungeon.get_visible_dungeon_string()) # print whole dungeon, debugging purpose now
 
         while True:  # another while game loop
-            # print(f"{dungeon.get_room(dungeon.adventurer_loc)}")
-            # print(f"{dungeon.adventurer_loc}")
-            # print(f"{dungeon.exit}")
-
-            # if 1. alive 2. get the 4 pillars  3.arrived at the exit
-
             if adventurer.is_alive() and \
                     adventurer.has_all_pillars() and \
                     dungeon.adventurer_loc == dungeon.exit:
-                print("Congrats! you win the game!")
+                print("\n:) Congrats! you win the game!")
                 break
             elif not adventurer.is_alive():
-                print("Your adventurer is dead, better luck next time!")
+                print("\n:( Uh-oh, better luck next time!")
                 break
             else:
-                move_or_command = input("Please input your move or command: ")
-                if move_or_command.lower() == "p":
+                move_or_command = (input("Your move or command: ")).lower()
+                if move_or_command == "p":
                     if adventurer.health_pots > 0:
                         health_potion_created = PotionFactory.create_potion("health")
                         adventurer.use_health_potion()
                         heal_num = health_potion_created.heal_amount
-                        print("You used a health potion, which healed your adventurer "+ str(heal_num)+" hit points.")
+                        print(adventurer.name+" used a health potion, which healed "+ str(heal_num)+" hit points.")
                         adventurer.heal_adventurer(heal_num)
                     else:
-                        print("No health potion in your inventory yet.")
+                        print("No health potion in "+adventurer.name+"'s inventory yet.")
                         continue
-                elif move_or_command.lower() == "v":
+                elif move_or_command == "v":
                     # dungeon.total_rows  # 5
                     # dungeon.total_columns  # 5
                     # dungeon.adventurer_loc #(4,1)
-                    for i in range(dungeon.adventurer_loc[0]-1, dungeon.adventurer_loc[0]+2):
-                        for j in range(dungeon.adventurer_loc[1]-1, dungeon.adventurer_loc[1]+2):
-                            if self.room_in_bound(i,j,dungeon):
-                                map_one.set_visited_room(i,j)
-                    print("You used a vision potion, which revealed all the valid rooms around your adventurer.")
-                    print(dungeon.get_visible_dungeon_string(map_one.visited_array()))
-                elif move_or_command.lower() == "i":   # i: show adventurer info
-                    print("Adventurer status listed below: \n" + f"{adventurer}")
-                elif move_or_command.lower() == "m":   # m: show adventurer map
+                    if adventurer.vision_pots > 0:
+                        for i in range(dungeon.adventurer_loc[0]-1, dungeon.adventurer_loc[0]+2):
+                            for j in range(dungeon.adventurer_loc[1]-1, dungeon.adventurer_loc[1]+2):
+                                if self.room_in_bound(i,j,dungeon):
+                                    map_one.set_visited_room(i,j)
+                        print(adventurer.name+" used a vision potion, which revealed all valid adjacent rooms.")
+                        print(dungeon.get_visible_dungeon_string(map_one.visited_array()))
+                    else:
+                        print("No vision potion in " + adventurer.name + "'s inventory yet.")
+                        continue
+                elif move_or_command == "i":   # i: show adventurer info
+                    print(adventurer.name+"'s status listed below: \n" + f"{adventurer}")
+                elif move_or_command == "m":   # m: show adventurer map
                     print("Currently visited rooms displayed below: ")
                     map_one.set_visited_room(dungeon.adventurer_loc[0], dungeon.adventurer_loc[1])
                     print(dungeon.get_visible_dungeon_string(map_one.visited_array()))
-                elif move_or_command.lower() == "w":   # m: show whole dungeon
+                elif move_or_command == "w":   # m: show whole dungeon
                     print("Whole dungeon displayed below: ")
                     print(dungeon.get_visible_dungeon_string()) # print whole dungeon, debugging purpose now
-                elif move_or_command.lower() == "q":  # quit
+                elif move_or_command == "q":  # quit
                     print("Thanks,Bye!")
                     break
-                elif move_or_command.lower() in ("north", "south", "west", "east"):
-                    new_room = dungeon.move_adventurer(move_or_command.lower().lower())
-
-                    print("New_room displayed below: \n" + f"{new_room}")
+                elif move_or_command in ("north", "south", "west", "east"):
+                    # if dungeon.move_adventurer(move_or_command):
+                    if dungeon.get_room(dungeon.adventurer_loc).get_door(move_or_command):
+                        new_room = dungeon.move_adventurer(move_or_command)
+                        print("New_room displayed below: \n" + f"{new_room}")
+                    else:
+                        print("No doors in that direction, choose again")
+                        continue
 
                     if new_room.health_potion:
                         print(adventurer.name + " found " + f"{new_room.health_potion}" + " health potion!")
@@ -161,57 +169,18 @@ Good luck!!!
                         adventurer.add_pillar(pillar_str)
                     if new_room.pit_damage > 0:
                         adventurer.damage_adventurer(new_room.pit_damage)
-
-
-
-
-
-                # elif move_or_command.lower() == "south":
-                #     new_room = dungeon.move_adventurer("south")
-                #     print("New_room displayed below: \n" + f"{new_room}")
-                # elif move_or_command.lower() == "west":
-                #     new_room = dungeon.move_adventurer("west")
-                #     print("New_room displayed below: \n" + f"{new_room}")
-                # elif move_or_command.lower() == "east":
-                #     new_room = dungeon.move_adventurer("east")
-                #     print("New_room displayed below: \n" + f"{new_room}")
                 else:
                     print("Invalid input, choose again.")
+                    print("\'i\' for adventurer status\n"
+                          "\'m\' for current traveled room/ map\n"
+                          "\'w\' for whole dungeon map\n"
+                          "\'p\' for using health potion\n"
+                          "\'v\' for using vision potion\n"
+                          "\'q\' for quit the game\n"
+                          "or \'north\', \'east\', etc."
+                          )
                     continue
 
-
-
-                # print the new room status(H/V/P/M)
-                # print(
-                #       "New_room health potion #: " + f"{new_room.health_potion}" + "\n" +
-                #       "New_room vision potion #: " + f"{new_room.vision_potion}" + "\n" +
-                #       "New_room pit damage: " + f"{new_room.pit_damage}" + "\n" +
-                #       "New_room contents: " + f"{new_room.contents}" + "\n"
-                #     )
-
-                # if new_room.health_potion:
-                #     print(adventurer.name+" found " + f"{new_room.health_potion}" + " health potion!")
-                # if new_room.vision_potion:
-                #     print(adventurer.name+" found " + f"{new_room.vision_potion}" + " vision potion!")
-                # if new_room.contents:
-                #     if new_room.contents == "A":
-                #         print(adventurer.name+" found pillar of Abstraction!")
-                #     elif new_room.contents == "P":
-                #         print(adventurer.name+" found pillar of Polymorphism!")
-                #     elif new_room.contents == "I":
-                #         print(adventurer.name+" found pillar of Inheritance!")
-                #     elif new_room.contents == "E":
-                #         print(adventurer.name+" found pillar of Encapsulation!")
-                # adventurer.add_potions(dungeon.collect_potions())
-                # pillar_str = dungeon.collect_pillars()
-                # if pillar_str:
-                #     adventurer.add_pillar(pillar_str)
-                # if new_room.pit_damage > 0:
-                #     adventurer.damage_adventurer(new_room.pit_damage)
-
-                # report the most current status to the player, eg: print(f"{adventurer}")
-                # print adventurer status
-                # print("Adventurer status listed below: \n" + f"{adventurer}")
 
     def room_in_bound(self, row, col, dungeon):
         if row < 0 or row > dungeon.total_rows:
@@ -221,46 +190,24 @@ Good luck!!!
         else:
             return True
 
-    def new_room_updates(self, *args):
-        pass
-        # new_room = dungeon.move_adventurer(*args)
-        # if new_room.health_potion:
-        #     print(adventurer.name + " found " + f"{new_room.health_potion}" + " health potion!")
-        # if new_room.vision_potion:
-        #     print(adventurer.name + " found " + f"{new_room.vision_potion}" + " vision potion!")
-        # if new_room.contents:
-        #     if new_room.contents == "A":
-        #         print(adventurer.name + " found pillar of Abstraction!")
-        #     elif new_room.contents == "P":
-        #         print(adventurer.name + " found pillar of Polymorphism!")
-        #     elif new_room.contents == "I":
-        #         print(adventurer.name + " found pillar of Inheritance!")
-        #     elif new_room.contents == "E":
-        #         print(adventurer.name + " found pillar of Encapsulation!")
-        # adventurer.add_potions(dungeon.collect_potions())
-        # pillar_str = dungeon.collect_pillars()
-        # if pillar_str:
-        #     adventurer.add_pillar(pillar_str)
-        # if new_room.pit_damage > 0:
-        #     adventurer.damage_adventurer(new_room.pit_damage)
-
-
     def print_complete_menu(self):
         print(
-              "\nMovement commands: \n"
-              "\tnorth: move up\n"
-              "\tsouth: move down\n"
-              "\twest: move left\n"
-              "\teast: move right\n"
-              "Status commands: \n"
+
+              "\nStatus commands: \n"
               "\ti: show adventurer info\n"
               "\tm: show adventurer map\n"
+              "\tw: show whole dungeon map\n"
               "Item commands: \n"
               "\tp: use health potion\n"
               "\tv: use vision potion\n"
               "Other commands: \n"
               "\tq: quit the game\n"
-              "----to be continue----\n")
+              "Movement commands: \n"
+              "\tnorth: move up\n"
+              "\tsouth: move down\n"
+              "\twest: move left\n"
+              "\teast: move right\n"
+        )
 
 if __name__ == "__main__":
     main = Main()
