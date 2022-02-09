3dac0f29a1074a3c16af60376674effe8a93391a
308241561 <huangxingguo1986@gmail.com>
2022-02-09 13:08:49 -0800

    added button window

--- dungeon_adventure_GUI.py
+++ dungeon_adventure_GUI.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import filedialog
 
 class dungeon_adventure_GUI:
 
@@ -14,15 +15,25 @@ class dungeon_adventure_GUI:
     def create_new_game_window(self):
         # checkout this site https://www.youtube.com/watch?v=tpwu5Zb64lQ
         global pop  # to make it accessiable to other functions, otherwise tkinter won't work in our way
-        top = Toplevel(self.root)
-        # top.mainloop()
+        pop = Toplevel(self.root)
+        pop.title("New Game")
+        pop.geometry("600x300")
+        btn2 = Button(pop, text="Confirm New Game").place(relx=0.75,rely=0.9)
+
+
+    def load_existing_game_window(self):
+        global pop  # to make it accessiable to other functions, otherwise tkinter won't work in our way
+        pop = Toplevel(self.root)
+        pop.title("Load Game")
+        pop.geometry("600x300")
+        btn3 = Button(pop, text="Load Gxisting Game").place(relx=0.75, rely=0.9)
 
     def welcome_window(self):
 
         canvas = self.welcome_screen_canvas
 
-        new_game_btn = Button(canvas, text="New Game", command=self.create_new_game_window()).place(relx=0.5,rely=0.5)
-        load_game_btn = Button(canvas, text="Load Game", command='Need a command').place(relx=0.5, rely=0.6)
+        new_game_btn = Button(canvas, text="New Game", command=self.create_new_game_window).place(relx=0.5,rely=0.5)
+        load_game_btn = Button(canvas, text="Load Game", command=self.load_existing_game_window).place(relx=0.5, rely=0.6)
         quit_game_btn = Button(canvas, text="Quit Game", command=self.root.destroy).place(relx=0.5, rely=0.7)
 
         global img # to make it accessiable to other functions, otherwise tkinter won't work in our way
