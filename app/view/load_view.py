540f46d0f214527b2d1e0e463e71cab2b0178122
Stephanie Liu <stephanieleighliu@gmail.com>
2022-03-11 11:05:23 -0800

    Minor refractor for readability in load_view.py and PEP8 conformity.

--- app/view/load_view.py
+++ app/view/load_view.py
@@ -1,16 +1,17 @@
-import tkinter as tk
 from tkinter import *
+
 
 class LoadView:
     """
     View for loading games from the save db.  Displays each save as a separate button with basic user information.
     """
-    def setup(self, controller, parent):
+    @staticmethod
+    def setup(controller, parent):
         games = controller.get_saved_games()
-        pop2 = Toplevel(parent)
-        pop2.geometry("800x450")
-        pop2.resizable(width=False, height=False)
-        pop2.title("Select a Game to Load")
+        load_screen = Toplevel(parent)
+        load_screen.geometry("800x450")
+        load_screen.resizable(width=False, height=False)
+        load_screen.title("Select a Game to Load")
         button_text, button_dict = {}, {}
         curr_x, curr_y = 0.01, 0.02
         for game in games:
@@ -21,8 +22,8 @@ class LoadView:
         for timestamp in button_text:
             def func(x=timestamp):
                 return controller.load_game_to_model(x)
-            button_dict[timestamp] = Button(pop2, text=button_text[timestamp], command=func)
+            button_dict[timestamp] = Button(load_screen, text=button_text[timestamp], command=func)
             button_dict[timestamp].place(relx=curr_x, rely=curr_y)
             curr_y += 0.05
-        btn3 = Button(pop2, text="Cancel", command=lambda: pop2.destroy())
+        btn3 = Button(load_screen, text="Cancel", command=lambda: load_screen.destroy())
         btn3.place(relx=0.75, rely=0.9)
